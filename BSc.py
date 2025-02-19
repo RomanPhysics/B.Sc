c365b6f8ca08640e5d71debd1d3c574620cfe2be
RomanUUPhysics <roman.sultanov@hotmail.com>
2025-02-19 13:18:53 +0100

    Update BSc.py

--- BSc.py
+++ BSc.py
@@ -2,8 +2,9 @@
 #Imports
 import numpy as np
 from iminuit import Minuit
-from numba import njit
+from numba import njit, prange
 import time
+
 np.set_printoptions(precision=19)
 np.random.seed(6)
 
@@ -11,45 +12,57 @@ np.random.seed(6)
 #Probabiliy Distribution Function
 @njit
 def PDF(costL, pL, costp, pp, Px, Py, Pz, aXi, aL, pXi):
-    Lx = np.sin(np.arccos(costL))*np.cos(pL)
-    Ly = np.sin(np.arccos(costL))*np.sin(pL)
+    sintL = np.sqrt(1.0 - costL * costL)
+    Lx = sintL*np.cos(pL)
+    Ly = sintL*np.sin(pL)
     Lz = costL
-    px = np.sin(np.arccos(costp))*np.cos(pp)
-    py = np.sin(np.arccos(costp))*np.sin(pp)
+
+    sintp = np.sqrt(1.0 - costp * costp)
+    px = sintp*np.cos(pp)
+    py = sintp*np.sin(pp)
     pz = costp
+
     bXi = np.sqrt(1-aXi*aXi)*np.sin(pXi)
     yXi = np.sqrt(1-aXi*aXi)*np.cos(pXi)
+
     PL1 = ((aXi+Px*Lx+Py*Ly+Pz*Lz)*Lx + bXi*(Py*Lz-Pz*Ly) + yXi*(Ly*(Px*Ly-Py*Lx)-Lz*(Pz*Lx-Px*Lz)))/(1+aXi*(Px*Lx+Py*Ly+Pz*Lz))
     PL2 = ((aXi+Px*Lx+Py*Ly+Pz*Lz)*Ly + bXi*(Pz*Lx-Px*Lz) + yXi*(Lz*(Py*Lz-Pz*Ly)-Lx*(Px*Ly-Py*Lx)))/(1+aXi*(Px*Lx+Py*Ly+Pz*Lz))
     PL3 = ((aXi+Px*Lx+Py*Ly+Pz*Lz)*Lz + bXi*(Px*Ly-Py*Lx) + yXi*(Lx*(Pz*Lx-Px*Lz)-Ly*(Py*Lz-Pz*Ly)))/(1+aXi*(Px*Lx+Py*Ly+Pz*Lz))
+
     z1 = Lx; z2 = Ly; z3 = Lz
     x1 = (Py*Lz-Pz*Ly)/np.sqrt((Py*Lz-Pz*Ly)**2 + (Pz*Lx-Px*Lz)**2 + (Px*Ly-Py*Lx)**2)
     x2 = (Pz*Lx-Px*Lz)/np.sqrt((Py*Lz-Pz*Ly)**2 + (Pz*Lx-Px*Lz)**2 + (Px*Ly-Py*Lx)**2)
     x3 = (Px*Ly-Py*Lx)/np.sqrt((Py*Lz-Pz*Ly)**2 + (Pz*Lx-Px*Lz)**2 + (Px*Ly-Py*Lx)**2)
     y1 = z2*x3-z3*x2; y2 = z3*x1-z1*x3; y3 = z1*x2-z2*x1
+
     PLz = PL1*z1 + PL2*z2 + PL3*z3
     PLx = PL1*x1 + PL2*x2 + PL3*x3
     PLy = PL1*y1 + PL2*y2 + PL3*y3
+
     pdf = (1+aXi*(Px*Lx + Py*Ly + Pz*Lz))*(1+aL*(PLx*px + PLy*py + PLz*pz))/(4*np.pi)**2
     return pdf
 
 #Monte-Carlo Generation
 @njit
 def MonteCarlo(n, Px, Py, Pz, aXi, aL, pXi):
-    data=[]
-    while len(data) < n:
-        costL = np.random.uniform(-1, 1)
-        pL = np.random.uniform(-np.pi, np.pi)
-        costp = np.random.uniform(-1, 1)
-        pp = np.random.uniform(-np.pi, np.pi)
-        pdf = PDF(costL, pL, costp, pp, Px, Py, Pz, aXi, aL, pXi)
+    data = np.empty((n, 4))
+    count = 0
+    while count < n:
+        costL = np.random.uniform(-1.0, 1.0)
+        pL    = np.random.uniform(-np.pi, np.pi)
+        costp = np.random.uniform(-1.0, 1.0)
+        pp    = np.random.uniform(-np.pi, np.pi)
+
+        pdfval = PDF(costL, pL, costp, pp, Px, Py, Pz, aXi, aL, pXi)
+
         y = np.random.uniform(0, 0.0159)  #from 0 to maxvalue of PDF
-        if pdf>0.0159:
-            print(pdf)
-            print([costL, pL, costp, pp])
-        if y<pdf:
-            data.append([costL, pL, costp, pp])
-    return np.array(data)
+        if y < pdfval:
+            data[count, 0] = costL
+            data[count, 1] = pL
+            data[count, 2] = costp
+            data[count, 3] = pp
+            count += 1
+    return data
 
 #Number of events
 N=10000000
@@ -68,7 +81,10 @@ angles = MonteCarlo(N, Px, Py, Pz, aXi, aL, pXi)
 #Negative Log-Likelihood Function
 @njit(fastmath=True, nogil=True)
 def NLL(Px, Py, Pz, aXi, aL, pXi):
-    logL = np.sum(np.log(PDF(angles[:,0], angles[:,1], angles[:,2], angles[:,3], Px, Py, Pz, aXi, aL, pXi)))
+    logL = 0.0
+    for i in prange(angles.shape[0]):
+        pdfval = PDF(angles[i, 0], angles[i, 1], angles[i, 2], angles[i, 3], Px, Py, Pz, aXi, aL, pXi)
+        logL += np.log(pdfval)
     return -logL
 
 #-----------------------------------------------------------------------------------------------------------------------
@@ -92,6 +108,9 @@ end_time = time.time()
 totalseconds=end_time - start_time
 print(f"Time taken: {totalseconds//60} m, {totalseconds%60} s.")
 
+print("Fitted parameter values:")
 print(m.values)
+print("Fitted parameter errors:")
 print(m.errors)
+print("Covariance matrix:")
 print(m.covariance)
